bdf21ab0811dd98bf03bba995f235de45d7636dc
Christian Eckardt <eckardt.christian@web.de>
2020-11-29 13:16:29 +0100

    updating main page, title and little improvements

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ function App() {
       <Navigation />
       <br></br>
       <br></br>
-      <div style={{display: "flex", flexDirection: "row", flexWrap:"wrap", maxWidth:"60vw"}}>
+      <div style={{display: "flex", flexDirection: "row", flexWrap:"wrap", maxWidth:"60vw", justifyContent: "center"}}>
       <BlogEntry blog={{image:"https://1thingtodo.de/wp-content/uploads/2016/02/barcelona-reisebericht-1-thing-to-do.jpg", 
                         title:"My Trip to Barcelona", portrait: "https://i.pinimg.com/564x/ce/2a/95/ce2a95e99faceaf7af19c273b10ebcc1.jpg", date:"10-10-2020", author:"Chris", postdate:"11-11-2020"}}/>
       <BlogEntry blog={{image:"https://savorylens.com/wp-0463a-content/uploads/2018/01/Prag-45.jpg", 
-                        title:"My Trip to Prague", portrait: "https://i.pinimg.com/564x/ce/2a/95/ce2a95e99faceaf7af19c273b10ebcc1.jpg", date:"10-10-2020", author:"Chris P Chicken", postdate:"11-11-2020"}}/>
+                        title:"My Trip to Prague", portrait: "https://i.pinimg.com/564x/ce/2a/95/ce2a95e99faceaf7af19c273b10ebcc1.jpg", date:"10-11-2020", author:"Chris P Chicken", postdate:"11-11-2020"}}/>
       <BlogEntry blog={{image: "https://www.logistik-watchblog.de/images/2018/03/shutterstock_139999093-1.jpg", 
-                        title:"My Trip to London", portrait: "https://www.finetoshine.com/wp-content/uploads/2020/07/Lisa-Simpson-aesthetic-profile-pic.jpg", date:"10-10-2020", author:"Chris P Bacon", postdate:"11-11-2020"}}/>
+                        title:"My Trip to London", portrait: "https://www.finetoshine.com/wp-content/uploads/2020/07/Lisa-Simpson-aesthetic-profile-pic.jpg", date:"10-12-2020", author:"Chris P Bacon", postdate:"11-11-2020"}}/>
+      <BlogEntry blog={{image: "https://lovingnewyork.de/wp-content/uploads/2017/05/New-York-Kreuzfahrt2-1600x960.jpg", 
+                        title:"My Trip to New York", portrait: "https://www.finetoshine.com/wp-content/uploads/2020/07/Lisa-Simpson-aesthetic-profile-pic.jpg", date:"10-13-2020", author:"Chris P Bacon", postdate:"11-11-2020"}}/>
       </div>
       
     </div>
--- src/Blogentry.js
+++ src/Blogentry.js
@@ -3,17 +3,17 @@ import React from "react";
 
 const BlogEntry = ({blog}) => {
     return(
-        <div style={{width: "220px", margin: "2%", backgroundColor:"white", border:"0.5px solid rgb(211,211,211,0.8)"}}>
-            <center><img alt="image from vacation" style={{width: "100%", height: "150px", objectFit:"cover", margin:"0"}}src={blog.image}></img></center>
+        <div style={{width: "300px", margin: "2%", backgroundColor:"white", border:"0.5px solid rgb(211,211,211,0.8)"}}>
+            <center><img alt="image from vacation" style={{width: "100%", height: "200px", objectFit:"cover", margin:"0"}}src={blog.image}></img></center>
             <div style={{backgroundColor:"white", padding:"1% 1% 0 1%"}}>
-            <h4 style={{margin:"0"}}>{blog.title}
+            <h3 style={{margin:"0"}}>{blog.title}
             <br></br>
-            <span style={{fontSize: "0.5em"}}>date: {blog.date}</span>
-            </h4>
+            <span style={{fontSize: "0.6em"}}>travel date: {blog.date}</span>
+            </h3>
             <h5>
             <img alt="profile" style={{height: "30px", width:"30px", objectFit:"cover", borderRadius: "50%", verticalAlign:"middle", float:"left", marginRight: "10px"}} src={blog.portrait}></img> 
             {blog.author}<br></br>
-            <span style={{fontSize:"0.6em"}}>created: {blog.postdate}</span>
+            <span style={{fontSize:"0.7em"}}>created: {blog.postdate}</span>
             </h5>
          
 
