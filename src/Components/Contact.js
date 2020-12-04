40f79aa15a3b0ccaf99672e4eecd44460d9c8275
Christian Eckardt <eckardt.christian@web.de>
2020-12-04 21:34:42 +0100

    updating design and form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,16 +1,17 @@
-import chris from "./Images/chris2.png";
+
 import {Component} from "react";
 import Navigation from "./Navigation";
 import illustration from "./Images/contact.svg";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 export class Contactpage extends Component {
     render(){
         return(
             <>
             <Navigation />
-            <br></br>
-            <div style={{backgroundColor:"#00BCD4", width:"100vw", maxWidth:"700px", fontSize:"1.5em", color:"white", margin: "20px auto"}}>
+            <div style={{backgroundColor:"#00BCD4", maxWidth:"100vw", paddingTop:"20px", margin:"0"}}>
+            <div style={{backgroundColor:"#00BCD4", maxWidth:"700px", fontSize:"1.5em", color:"white", margin: "20px auto", paddingBottom:"120px"}}>
                 
                 <h2 style={{paddingLeft:"20px", paddingTop:"20px"}}>Contact</h2>
                 <p style={{paddingLeft:"20px", marginBottom:"-20px"}}>
@@ -18,12 +19,16 @@ export class Contactpage extends Component {
                     Zesenstraße 9 <br></br>
                     22301 Hamburg <br></br>
                     <br></br>
-                    Email: eckardt.christian@web.de <br></br>
-                    Phone: 0178 5325488
+                    <FontAwesomeIcon icon={faEnvelope} /> &nbsp; eckardt.christian@web.de <br></br>
+                    <FontAwesomeIcon icon={faPhone} /> &nbsp; 0178 5325488
                     
                 </p>
                 <img style={{width: "90vw", maxWidth: "700px"}} src={illustration} alt="contact information"></img>
             </div>
+
+
+            </div>
+           
             </>
         )
 
--- src/Components/NewPost.js
+++ src/Components/NewPost.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Navigation from "./Navigation";
 import Travelers from "./Images/travelers.svg";
+import { Link } from 'react-router-dom';
 
 
 export class NewPost extends Component {
@@ -9,34 +10,41 @@ export class NewPost extends Component {
             <>
             
             <Navigation />
-            <div style={{display:"flex", flexDirection:"row", flexWrap:"wrap", alignItems:"center", justifyContent:"center", backgroundColor:"#00BCD4", height:"200%"}}>
+      
             
-            <form style={{backgroundColor:"#00BCD4", padding:"20px", width:"100%", maxWidth: "500px", textAlign:"center", marginBottom:"20px"}}>
+            <div style={{display:"flex", flexDirection:"row", flexWrap:"wrap", alignItems:"center", justifyContent:"center", backgroundColor:"#00BCD4", height:"200%"}}>
+
+            
+
+            <form style={{backgroundColor:"#00BCD4", padding:"20px", width:"100%", maxWidth: "500px", textAlign:"center", marginBottom:"20px", color:"white"}}>
+            
+            <h2>Create new blog entry</h2>
                 <label>Title</label>
                 <br></br>
-                <input type="text" placeholder="Enter a title for your post"></input>
+                <input style={{marginBottom:"10px", width: "200px", padding:"10px", borderStyle:"none"}} type="text" placeholder="Enter a title for your post"></input>
                 <br></br>
                 <label>Travel Date</label>
                 <br></br>
-                <input type="date"></input>
+                <input type="date" style={{marginBottom:"10px", width: "200px", padding:"10px", borderStyle:"none"}}></input>
                 <br></br>
                 <label>Image Link</label>
                 <br></br>
-                <input type="url" placeholder="Enter an image link"></input>
+                <input type="url" placeholder="Enter an image link" style={{marginBottom:"10px", width: "200px", padding:"10px", borderStyle:"none"}}></input>
                 <br></br>
                 <label>Longitude</label>
                 <br></br>
-                <input type="number" placeholder="Enter longitude"></input>
+                <input type="number" placeholder="Enter longitude" style={{marginBottom:"10px", width: "200px", padding:"10px", borderStyle:"none"}}></input>
                 <br></br>
                 <label>Latitude</label>
                 <br></br>
-                <input type="number" placeholder="Enter latitude"></input>
+                <input type="number" placeholder="Enter latitude" style={{marginBottom:"10px", width: "200px", padding:"10px", borderStyle:"none"}}></input>
                 <br></br>
                 <label>Description</label>
                 <br></br>
-                <textarea cols="30" rows="10" placeholder="Here you can describe your trip"></textarea>
+                <textarea cols="30" rows="8" placeholder="Here you can describe your trip" style={{marginBottom:"10px", width: "200px", padding:"10px", borderStyle:"none"}}></textarea>
                 <br></br>
-                <input type="submit" value="Save"></input>
+                <input type="submit" value="Save" style={{margin:"10px", padding: "8px", backgroundColor: "#32CD32", borderStyle: "none", borderRadius:"20px", width:"90px", color: "white"}}></input>
+                <Link to="/"><button style={{margin:"10px", padding: "8px", backgroundColor: "#FF8C00", borderStyle: "none", borderRadius:"20px", width:"90px", color: "white"}}>Cancel</button></Link>
 
 
             </form>
