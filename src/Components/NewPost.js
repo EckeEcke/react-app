0af18499ba6062e95aa34ea8ab510077d9f98de4
Christian Eckardt <eckardt.christian@web.de>
2020-12-04 18:57:38 +0100

    updating newpost component

--- src/Components/NewPost.js
+++ src/Components/NewPost.js
@@ -1,13 +1,20 @@
 import React, {Component} from "react";
+import Navigation from "./Navigation";
+import Travelers from "./Images/travelers.svg";
 
 
 export class NewPost extends Component {
     render(){
         return(
-            <form>
+            <>
+            
+            <Navigation />
+            <div style={{display:"flex", flexDirection:"row", flexWrap:"wrap", alignItems:"center", justifyContent:"center", backgroundColor:"#00BCD4", height:"200%"}}>
+            
+            <form style={{backgroundColor:"#00BCD4", padding:"20px", width:"100%", maxWidth: "500px", textAlign:"center", marginBottom:"20px"}}>
                 <label>Title</label>
                 <br></br>
-                <input type="text"></input>
+                <input type="text" placeholder="Enter a title for your post"></input>
                 <br></br>
                 <label>Travel Date</label>
                 <br></br>
@@ -15,25 +22,32 @@ export class NewPost extends Component {
                 <br></br>
                 <label>Image Link</label>
                 <br></br>
-                <input type="url"></input>
+                <input type="url" placeholder="Enter an image link"></input>
                 <br></br>
                 <label>Longitude</label>
                 <br></br>
-                <input type="number"></input>
+                <input type="number" placeholder="Enter longitude"></input>
                 <br></br>
                 <label>Latitude</label>
                 <br></br>
-                <input type="number"></input>
+                <input type="number" placeholder="Enter latitude"></input>
                 <br></br>
                 <label>Description</label>
                 <br></br>
-                <input type="text"></input>
+                <textarea cols="30" rows="10" placeholder="Here you can describe your trip"></textarea>
                 <br></br>
                 <input type="submit" value="Save"></input>
 
 
             </form>
 
+            
+
+            <img src={Travelers} alt="traveling people" style={{height:"400px"}}></img>
+
+            </div>
+            
+            </>
         )
         }
     }
