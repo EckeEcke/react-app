e8811c689fac13c603d963e0dccecdcf655bc142
Christian Eckardt <eckardt.christian@web.de>
2020-12-05 23:44:20 +0100

    adding redirect file

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -6,10 +6,9 @@ function Description(){
 
     <div style={{maxWidth: "698px", width:"99.8%", backgroundColor:"#00BCD4", color:"white", border:"1px solid #00BCD4"}}>
   <p style={{padding:"5%", fontSize:"1.2em"}}>
-  Hello visitors,
-  <br></br><br></br>
+  <h4>About:</h4>
   <img style={{float:"right", width: "100px", margin:"10px"}} src={airport} alt="person waiting for airplane"></img>
-  welcome to my Travel Blog Editor built in React. This is my final project of the Full Stack Web Developer course at Hamburg Coding School. The rest is just some placeholder text
+   Welcome to my Travel Blog Editor! I built this SPA in React as my final project of the Full Stack Web Developer course at Hamburg Coding School. The rest is just some placeholder text
    until I know which features I managed to implement. Probably quite a few.
 
   </p>
